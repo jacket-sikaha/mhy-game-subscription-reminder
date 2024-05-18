697e0a0f1138aa074dd5a570efe1ede63502d8e2
sikara <xuhaoxian@gdlinkjob.com>
2024-05-18 11:07:26 +0800

    nextjs serverless函数问题cron使用异常 暂时不用nextjs

--- src/app/api/subscription/route.ts
+++ src/app/api/subscription/route.ts
@@ -2,15 +2,15 @@ import { logger } from "@/logger/pino-config";
 import { getShanghaiDate } from "@/logger/util";
 import myCronJob, { a, e, job, s } from "@/schedule/cron";
 
-export async function GET(request: Request) {
+export async function POST(request: Request) {
   // const { a } = await request.json();
   console.log(getShanghaiDate());
-  console.log(myCronJob);
   if (myCronJob.running) {
     myCronJob.e();
   } else {
     myCronJob.s();
   }
+  console.log(myCronJob.running);
   return Response.json({
     date: getShanghaiDate(),
   });
