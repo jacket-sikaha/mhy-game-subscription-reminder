a6b591c973824ae6daae53ad3cd78f863ca8cba7
xuhaoxian <xuhaoxian@gdlinkjob.com>
2024-10-11 11:51:19 +0800

    feat 添加测试环境变量接口

--- src/ocr/ocr.controller.ts
+++ src/ocr/ocr.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 
 @Controller('ocr')
-export class OcrController {}
+export class OcrController {
+  @Post('env')
+  getENV(): string {
+    return JSON.stringify({
+      AK: process.env.BDAK,
+      SK: process.env.BDSK,
+    });
+  }
+}
